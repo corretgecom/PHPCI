b3c44b06a5d95a23b159a7f77c756d57afc348f9
Àlex Corretgé <alex@viscaweb.com>
2015-02-06 15:21:50 +0100

    finalment fem que els widgets ocupin tota la grandaria, doncs si no, perdem informació de les classes i els arxius. Ara permetem executar la shell. Creat ticket pq aquest tipus de modificacions no afectin a codi versionat.

--- public/assets/js/build-plugins/phptallint.js
+++ public/assets/js/build-plugins/phptallint.js
@@ -1,6 +1,6 @@
 var phptalPlugin = ActiveBuild.UiPlugin.extend({
     id: 'build-phptal',
-    css: 'col-lg-3 col-md-12 col-sm-12 col-xs-12',
+    css: 'col-lg-12 col-md-12 col-sm-12 col-xs-12',
     title: 'PHPTAL Lint',
     lastData: null,
     box: true,
